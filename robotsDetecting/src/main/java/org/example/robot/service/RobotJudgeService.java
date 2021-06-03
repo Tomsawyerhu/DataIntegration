bba27da0d8ea9e38d8864626b46935f2f4524f47
hhc <1095549886@qq.com>
2021-06-04 00:36:48 +0800

    添加strategy工厂方法

--- robotsDetecting/src/main/java/org/example/robot/service/RobotJudgeService.java
+++ robotsDetecting/src/main/java/org/example/robot/service/RobotJudgeService.java
@@ -6,6 +6,7 @@ import org.example.robot.dao.LoginDao;
 import org.example.robot.model.ActionInfo;
 import org.example.robot.model.LoginInfo;
 import org.example.robot.strategy.AbstractRobotJudge;
+import org.example.robot.strategy.RobotJudgeFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class RobotJudgeService {
     LoginDao loginDao;
 
     public boolean isCertainRobot(String userId, RobotTypeEnums robotType){
-        AbstractRobotJudge judge=AbstractRobotJudge.getRobotJudgeByType(robotType);
+        AbstractRobotJudge judge= RobotJudgeFactory.getRobotJudgeByType(robotType);
         List<ActionInfo> actionInfos=actionDao.findActionsByUserId(userId);
         List<LoginInfo> loginInfos=loginDao.findLoginsByUserId(userId);
         return judge.isRobot(loginInfos,actionInfos);
